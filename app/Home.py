98d5904caa368673914bf550c2a4f697852cd32f
Jonathan Hollinger <jhollinger@fleetio.com>
2023-04-12 14:58:23 -0400

    Test Full Path Config

--- app/Home.py
+++ app/Home.py
@@ -7,9 +7,9 @@ def run():
 
     st.header("Fleetio Metrics")
 
-    st.write(f"<a style='text-decoration: none; margin:0px;' target='_self' href='/Page_1?param=1'>Page 1</a>", unsafe_allow_html=True)
-    st.write(f"<a style='text-decoration: none; margin:0px;' target='_self' href='/Page_2?param=1'>Page 2</a>", unsafe_allow_html=True)
-    st.write(f"<a style='text-decoration: none; margin:0px;' target='_self' href='/Page_3?param=1'>Page 3</a>", unsafe_allow_html=True) 
+    st.write(f"<a style='text-decoration: none; margin:0px;' target='_self' href='{st.secrets['base_url']}/Page_1?param=1'>Page 1</a>", unsafe_allow_html=True)
+    st.write(f"<a style='text-decoration: none; margin:0px;' target='_self' href='{st.secrets['base_url']}/Page_2?param=1'>Page 2</a>", unsafe_allow_html=True)
+    st.write(f"<a style='text-decoration: none; margin:0px;' target='_self' href='{st.secrets['base_url']}/Page_3?param=1'>Page 3</a>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     run()
